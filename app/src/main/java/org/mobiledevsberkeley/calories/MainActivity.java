5123781c5d183cd787ea65615378aa3136430bca
Gus Silva <gus7silva@yahoo.com>
2016-01-23 16:56:07 -0800

    Removed button
    removed calculate button, now the results update as input is updated.

--- app/src/main/java/org/mobiledevsberkeley/calories/MainActivity.java
+++ app/src/main/java/org/mobiledevsberkeley/calories/MainActivity.java
@@ -2,6 +2,9 @@ package org.mobiledevsberkeley.calories;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,37 +16,42 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private String TAG = "cs160";
-    private int workoutValue = 1;
-    TextView vUnits, vCalsBurned, vEquivCalsBurned;
-    EditText vNumber;
+    private int currentWorkoutValue, currentWorkoutPos, equivWorkoutPos;
+    private TextView vUnits, vEquivUnits, vCalsBurned, vEquivCalsBurned;
+    private EditText vNumber;
+    private Spinner equivalentSpinner;
+    private String[] workoutNames, unitNames = new String[2];
+    private int[] workoutVals, workoutUnits;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final String[] workoutNames = getResources().getStringArray(R.array.workout_name_array);
-        final String[] unitNames = {getResources().getString(R.string.reps), getResources().getString(R.string.minutes)};
-        final int[] workoutVals = getResources().getIntArray(R.array.workout_value_array);
-        final int[] workoutUnits = getResources().getIntArray(R.array.workout_unit_array);
-        workoutValue = workoutVals[0];
+        workoutNames = getResources().getStringArray(R.array.workout_name_array);
+        unitNames[0] = getResources().getString(R.string.reps);unitNames[1]=getResources().getString(R.string.minutes);
+        workoutVals = getResources().getIntArray(R.array.workout_value_array);
+        workoutUnits = getResources().getIntArray(R.array.workout_unit_array);
+        currentWorkoutPos = 0; equivWorkoutPos = 1;
+        currentWorkoutValue = workoutVals[currentWorkoutPos];
 
         vUnits = (TextView)findViewById(R.id.units_text);
-        vNumber = (EditText) findViewById(R.id.num_text);
+        vEquivUnits = (TextView)findViewById(R.id.equiv_units_text);
         vCalsBurned = (TextView)findViewById(R.id.calories_burned_amount);
         vEquivCalsBurned = (TextView)findViewById(R.id.equivalent_amount);
-
-        //Initialize button to calculate
-        Button calculateButton = (Button)findViewById(R.id.calculate_button);
-        calculateButton.setOnClickListener(new View.OnClickListener() {
+        vNumber = (EditText) findViewById(R.id.num_text);
+        vNumber.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v) {
-                String strAmount = vNumber.getText().toString();
-                int inputAmount = Integer.parseInt(strAmount);
-                inputAmount = (100 * inputAmount) / workoutValue;
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
 
-                if(vCalsBurned != null)
-                    vCalsBurned.setText(String.valueOf(inputAmount));
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                updateResults();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
             }
         });
 
@@ -56,10 +64,10 @@ public class MainActivity extends AppCompatActivity {
         workoutSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                if(vUnits != null)
-                    vUnits.setText(String.valueOf(unitNames[workoutUnits[position]]));
-                workoutValue = workoutVals[position];
-
+                vUnits.setText(String.valueOf(unitNames[workoutUnits[position]]));
+                currentWorkoutPos = position;
+                currentWorkoutValue = workoutVals[position];
+                updateResults();
             }
 
             @Override
@@ -69,13 +77,62 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //Initialize Spinner to choose equivalent workout to compare
-        Spinner equivalentSpinner = (Spinner) findViewById(R.id.equivalent_workout_spinner);
+        equivalentSpinner = (Spinner) findViewById(R.id.equivalent_workout_spinner);
         ArrayAdapter<CharSequence> equivalentAdapter = ArrayAdapter.createFromResource(this,
                 R.array.workout_name_array, android.R.layout.simple_spinner_item);
         equivalentAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         equivalentSpinner.setAdapter(equivalentAdapter);
+        equivalentSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                equivWorkoutPos = position;
+                int equivalentAmount = getEquivAmount();
+                vEquivCalsBurned.setText(String.valueOf(equivalentAmount));
+                String units = unitNames[workoutUnits[position]] + " of";
+                vEquivUnits.setText(units);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
 
+    }
+
+    private void updateResults()
+    {
+        int calsBurned = getCalsBurned();
+        vCalsBurned.setText(String.valueOf(calsBurned));
+
+        //Set equivalent to next workout amount
+        if(currentWorkoutPos == equivWorkoutPos) {
+            equivWorkoutPos = (currentWorkoutPos + 1) % workoutNames.length;
+            equivalentSpinner.setSelection(equivWorkoutPos, true);
+        }
+        int equivalentAmount = getEquivAmount();
+        vEquivCalsBurned.setText(String.valueOf(equivalentAmount));
+        String units = unitNames[workoutUnits[equivWorkoutPos]] + " of";
+        vEquivUnits.setText(units);
+    }
+
+    private int getCalsBurned()
+    {
+        String inputText = vNumber.getText().toString();
+        if(inputText.isEmpty())
+            return 0;
+        double inputAmount = Integer.parseInt(inputText);
+        double calsBurned = (100 * inputAmount) / currentWorkoutValue;
+        return (int)Math.ceil(calsBurned);
+    }
+
+    private int getEquivAmount()
+    {
+        double calsBurned = getCalsBurned();
+        double equivalentWorkoutValue = workoutVals[equivWorkoutPos];
+        double equivalentAmount = (calsBurned * equivalentWorkoutValue) / 100;
+        return (int)Math.ceil(equivalentAmount);
     }
 
 }
